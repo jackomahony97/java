e87862f2e9cd30ded6e6d48cf6829317a1c8132c
jackomahony97 <37781244+jackomahony97@users.noreply.github.com>
2019-11-29 22:07:46 +0000

    Update IllegalRainfallDataSourceException.java
    Added exception chaining

--- src/src/cs3318/exceptions/IllegalRainfallDataSourceException.java
+++ src/src/cs3318/exceptions/IllegalRainfallDataSourceException.java
@@ -1,5 +1,14 @@
-package cs3318.exceptions;
+package exceptions;
 
+/**
+ * Exception thrown to indicate that the file is not found
+ * @author Jack O Mahony
+ */
 public class IllegalRainfallDataSourceException extends Exception {
-
+    /**
+     * Exception chaining
+     */
+    public IllegalRainfallDataSourceException(String mess){
+        super(mess);
+    }
 }
